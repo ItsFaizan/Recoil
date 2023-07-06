c2f0f815e723dbae82dcc922cfb46ac75687508b
ItsFaizan <faizanjavid71@gmail.com>
2023-07-06 19:32:39 +0500

    Selector used

--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
-import { recoilCount } from './State';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { recoilCount, recoilSelector,modifier } from './State';
 const AppTwo = () => {
   const [count, setCount] = useRecoilState(recoilCount);
+  const [mod, setMod] = useRecoilState(modifier);
+  const modifiedCount = useRecoilValue(recoilSelector);
   
 
  return (
   <div >
     
     <h1>Recoil Count from AppTwo: {count}</h1>
-    <button onClick={() => setCount(count + 1)}>Increment</button>
+    <input type="number" value= {mod} onChange={(e) => setMod(e.target.value)} />
+    <button onClick={() => setCount(count + 1)}>Increment modifiedCount is {modifiedCount }</button>
     <button onClick={() => setCount(count - 1)}>Decrement</button>
 
   </div>
--- src/State.js
+++ src/State.js
@@ -4,3 +4,17 @@ export const recoilCount = atom({
     key: 'count',
     default: 0,
 });
+
+export const modifier = atom({
+    key: 'modifier',
+    default: 0
+})
+
+export const recoilSelector = selector({
+    key: 'modified-count',
+    get: ({get}) => {
+        const count = get(recoilCount);
+        const currentmodifier = get(modifier);
+        return count * currentmodifier;
+    }
+})
